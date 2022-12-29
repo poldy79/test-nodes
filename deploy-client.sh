16e7b44cd652e8fb65cc01beecbd46965d76d321
Leonard Penzer <leonard@penzer.de>
2022-12-29 10:24:36 +0100

    Enable nesting for ffs-client containers

--- deploy-client.sh
+++ deploy-client.sh
@@ -9,5 +9,6 @@ IPETH2=$7
 TEMPLATE=/zp0/pve-templates/template/cache/debian-11-standard_11.0-1_amd64.tar.gz
 pct stop $VMID
 pct destroy $VMID
-pct create $VMID $TEMPLATE --storage zp0 --net0 name=eth0,bridge=vmbr3,ip=dhcp,ip6=auto,tag=$ID,type=veth --net1 name=eth1,bridge=vmbr4,firewall=0,ip6=$IPV6/64,type=veth --memory 512 --hostname $NAME
+pct create $VMID $TEMPLATE --features nesting=1 --storage zp0 --net0 name=eth0,bridge=vmbr3,ip=dhcp,ip6=auto,tag=$ID,type=veth --net1 name=eth1,bridge=vmbr4,firewall=0,ip6=$IPV6/64,type=veth --memory 512 --hostname $NAME
+
 
