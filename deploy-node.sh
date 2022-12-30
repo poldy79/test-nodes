0443e759ce0b0479ab3df1700df1aa8e2297b7ef
root <root@ffs10.freifunk-stuttgart.de>
2022-12-30 23:15:22 +0100

    Do not deploy ssh-key to node

--- deploy-node.sh
+++ deploy-node.sh
@@ -57,8 +57,6 @@ spawn qm terminal $VMID
 expect -re ".*]"
 send "\n\r"
 expect -re ".*#"
-send "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5jQU6UhGFfeQrEZ09cNjyFuOrOKZxslGGznblcr/SSjHGCtISk9Z4bGquMAuqcn4hd6xlT+SyRJaIivkAWFfzpUKFDtg4MyE47s82Ny0ZGHvP+I4BVQsjdwYFKZLK9iqmkqZ52YrgSSjbH1QKKHDqvYx97X2hZUDx96lNzQrZAxzr21UEIqxGTXjcrhCDy+g81gyHQLnPc/RgU28JKEtmm1yOWrlLyN5ylmmGrexyY2fo4asJIJ60+KWjbID7I0VDcCHV2g6GOkQBgBoY6VIX+3ipX3nN8ANdB24Vjf9906Vc+FQowQAFW/NxLRS6bS6LqwskTdkf2RHbPykuBrAl root@leela.selfhosted.de' > /etc/dropbear/authorized_keys\n\r"
-expect -re ".*#"
 send "uci set fastd.mesh_vpn.secret=$SECRET\n\r"
 expect -re ".*#"
 send {uci set gluon-setup-mode.@setup_mode[0].enabled='0'}
